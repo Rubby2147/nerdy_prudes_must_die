149d53cf7e3bc3ff5a5476e6d076d0495ef0699c
Rubby2147 <164878633+Rubby2147@users.noreply.github.com>
2024-08-16 12:10:24 -0300

    Add files via upload

---
+++ damage calc.py
@@ -0,0 +1,11 @@
+dado= int(input('quantos dados vai rolar:'))
+ataques= int(input('quantos ataques iguais vai fazer:'))
+bonus= int(input('qual seu bonus para acertar:'))
+ac=int(input('qual ac quer acertar:'))
+dano=int(input('dano por cada ataque:'))
+dano_uma_vez = int(input('dano em um ataque só:'))
+crit =int(input('dano em um critico:'))
+hc = (1-pow(((ac-bonus)/20),dado))
+uma_vez= pow((hc),ataques+dado)
+dano_por_round =(ataques*(hc*(dano))+crit/20)+uma_vez*dano_uma_vez
+print(dano_por_round)
