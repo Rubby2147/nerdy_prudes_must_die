dd900f93598dfc5ee8d8af67404cd891d5a94f34
Rubby2147 <164878633+Rubby2147@users.noreply.github.com>
2024-08-16 13:41:57 -0300

    Add files via upload

---
+++ programa/agendamento de reuniões.py
@@ -0,0 +1,7 @@
+pessoas= int(input('quantas pessoas vão participar da reunião:'))
+tipo_de_reunião= (input('reunião sera executiva ou normal:'))
+if pessoas <=5 and tipo_de_reunião=='normal':
+    print ('a reuiniaõ sera em sala pequena')
+elif pessoas >=6 and pessoas <15 and tipo_de_reunião=='normal':
+    print('a reuiniaõ sera em sala media')
+else: pessoas >=15 and tipo_de_reunião=='normal' or tipo_de_reunião=='executivo'
