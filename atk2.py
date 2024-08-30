a883dac130a415e9a51b60c5708c8fdb9aec9746
Rubby2147 <164878633+Rubby2147@users.noreply.github.com>
2024-08-30 09:44:39 -0300

    Update atk2.py

--- atk2.py
+++ atk2.py
@@ -3,6 +3,7 @@ def calc_damage (dado,ataques,bonus,dano,crit,ac):
     ataque=(ataques*(hc*(dano))+(dado/20)*crit)
     return ataque
 
+print('='*50)
 ac=int(input('qual ac quer acertar:'))
 atk = int(input('quantos ataques diferentes uns dos outros vai fazer:'))
 
@@ -14,10 +15,12 @@ for x in (range(atk)):
     dano=int(input('dano por cada ataque:'))
     crit =int(input('dano em um critico:'))
     print('='*50)
-    fatores  = [dado,ataques,bonus,dano,crit]
-    resultado = [calc_damage(*fatores,ac)]
-    resultados.append(resultado)
-    total_de_dano = sum(resultado)
+
+fatores  = [dado,ataques,bonus,dano,crit]
+
+resultado = [calc_damage(*fatores,ac)]
+resultados.append(resultado)
+total_de_dano = sum(resultado)
 
 print ('seu dano por turno é:',total_de_dano)
 print('='*50)
